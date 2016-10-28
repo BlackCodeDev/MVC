72e657ed862c7c4e27515fa80954630a290e1951
BlackCodeDev <donmondi@gmail.com>
2016-10-28 11:53:45 +0300

    Changes

--- BootstrapHelpers/BootstrapHelpers/BootstrapHelpers/Helpers.cs
+++ BootstrapHelpers/BootstrapHelpers/BootstrapHelpers/Helpers.cs
@@ -10,7 +10,17 @@ namespace BSMVC
 {
     public static class BSHelpers
     {
-        public static MvcHtmlString BSTextBoxFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
+        /// <summary>
+        /// Extension Method For Bootstrap 
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="helper">The type we extend</param>
+        /// <param name="expression">lamda expression eg model=>model.Username</param>
+        /// <param name="cssClass">By default is null which means that applies the default boootstrap rendering, if provided it also takes account a custom css class</param>
+        /// <returns></returns>
+        /// 
+        public static MvcHtmlString BSTextBoxFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression, string cssClass=null)
         {
             var fieldName = ExpressionHelper.GetExpressionText(expression);
             var fullBindingName = helper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(fieldName);
@@ -19,10 +29,12 @@ namespace BSMVC
             var metadata = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
             var value = metadata.Model;
             
+            string bsCssClass ="form-control";
+
             TagBuilder TextBoxTag = new TagBuilder("input");
             TextBoxTag.Attributes.Add("name", fieldName);
             TextBoxTag.Attributes.Add("id", fieldId);
-            TextBoxTag.Attributes.Add("class", "form-control");
+            TextBoxTag.Attributes.Add("class", cssClass == null ? bsCssClass : String.Format("{0} {1}",bsCssClass,cssClass));
             TextBoxTag.Attributes.Add("value", value == null ? "" : value.ToString());
 
             var validationAttributes = helper.GetUnobtrusiveValidationAttributes(fullBindingName, metadata);
