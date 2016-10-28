470b693c96bacb48a1b429abc3e4b38da8ec1edf
BlackCodeDev <donmondi@gmail.com>
2016-10-28 11:26:17 +0300

    TextBoxHelper

--- BootstrapHelpers/BootstrapHelpers/BootstrapHelpers/Class1.cs
+++ BootstrapHelpers/BootstrapHelpers/BootstrapHelpers/Class1.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
-namespace BootstrapHelpers
+namespace BSMVC
 {
-    public class Class1
+    public static class BSHelpers
     {
-        //lll
+        public static MvcHtmlString BSTextBoxFor<TModel, TValue>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TValue>> expression)
+        {
+            var fieldName = ExpressionHelper.GetExpressionText(expression);
+            var fullBindingName = helper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(fieldName);
+            var fieldId = TagBuilder.CreateSanitizedId(fullBindingName);
+
+            var metadata = ModelMetadata.FromLambdaExpression(expression, helper.ViewData);
+            var value = metadata.Model;
+            TagBuilder TextBoxTag = new TagBuilder("input");
+            TextBoxTag.Attributes.Add("name", fieldName);
+            TextBoxTag.Attributes.Add("id", fieldId);
+            TextBoxTag.Attributes.Add("class", "form-control");
+            TextBoxTag.Attributes.Add("value", value == null ? "" : value.ToString());
+
+
+
+            var validationAttributes = helper.GetUnobtrusiveValidationAttributes(fullBindingName, metadata);
+            foreach (var key in validationAttributes.Keys)
+            {
+                TextBoxTag.Attributes.Add(key, validationAttributes[key].ToString());
+            }
+
+            return new MvcHtmlString(TextBoxTag.ToString(TagRenderMode.SelfClosing));
+
+        }
+
     }
 }
